a138651449f8487853ac3c838bbe6bfd2c119930
leonardormlins <leonardormlins@gmail.com>
2021-01-30 16:09:03 -0300

    refact: Prevent error controller post recipe 	Add test and error treatment on create a new recipe

--- src/controllers/recipes.ts
+++ src/controllers/recipes.ts
@@ -1,13 +1,23 @@
 import { Controller, Post } from '@overnightjs/core';
 import { Recipe } from '@src/models/recipe';
 import { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import { resourceLimits } from 'worker_threads';
 
 @Controller('recipe')
 export class RecipeController {
   @Post('')
   public async create(req: Request, res: Response): Promise<void> {
-    const recipe = new Recipe(req.body);
-    const result = await recipe.save();
-    res.status(201).send(result);
+    try {
+      const recipe = new Recipe(req.body);
+      const result = await recipe.save();
+      res.status(201).send(result);
+    } catch (error) {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(422).send({ error: error.message });
+      } else {
+        res.status(500).send({ error: 'Internal Server Error' });
+      }
+    }
   }
 }
--- test/functional/recipe.test.ts
+++ test/functional/recipe.test.ts
@@ -1,4 +1,7 @@
+import { Recipe } from '@src/models/recipe';
+
 describe('Recipes functional tests', () => {
+  beforeAll(async () => await Recipe.deleteMany({}));
   describe('When creating a recipe', () => {
     it('should create a recipe with success', async () => {
       const newRecipe = {
@@ -26,5 +29,19 @@ describe('Recipes functional tests', () => {
       expect(response.status).toBe(201);
       expect(response.body).toEqual(newRecipe);
     });
+
+    it('should return 422 when there is a validation error', async () => {
+      const newRecipe = {};
+
+      const response = await global.testRequest.post('/recipe').send(newRecipe);
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        error:
+          'Recipe validation failed: author: Path `author` is required., ' +
+          'title: Path `title` is required.',
+      });
+    });
   });
+
+  describe('When creating a recipe', () => {});
 });
