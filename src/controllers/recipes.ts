57f8f626211b0b99a4831a47b88d4b903bb7476a
leonardormlins <leonardormlins@gmail.com>
2021-02-01 23:51:53 -0300

    refact: Move business rules from controller 	All access to database from controller was moved to a new directory called repository

--- src/controllers/recipes.ts
+++ src/controllers/recipes.ts
@@ -3,32 +3,19 @@ import { Recipe } from '@src/models/recipe';
 import { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import { resourceLimits } from 'worker_threads';
+import { create, findAll } from '@src/repositories/repice';
 
 @Controller('recipe')
 export class RecipeController {
   @Post('')
   public async create(req: Request, res: Response): Promise<void> {
-    try {
-      const recipe = new Recipe(req.body);
-      const result = await recipe.save();
-      res.status(201).send(result);
-    } catch (error) {
-      if (error instanceof mongoose.Error.ValidationError) {
-        res.status(422).send({ error: error.message });
-      } else {
-        res.status(500).send({ error: 'Internal Server Error' });
-      }
-    }
+    const response = await create(req);
+    res.status(response.code).send(response.recipe || response.error);
   }
 
   @Get('')
   public async getAllRecipes(_: Request, res: Response): Promise<void> {
-    try {
-      const recipes = await Recipe.find({});
-      res.status(200).send(recipes);
-    } catch (error) {
-      console.log(error);
-      res.status(500).send({ error: 'Internal Server Error' });
-    }
+    const response = await findAll();
+    res.status(response.code).send(response.recipe || response.error);
   }
 }
